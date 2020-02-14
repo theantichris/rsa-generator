21b3a50b3ac43aadd20eef4e5d45988d86bf50d4
Christopher Lamm <chris@theantichris.com>
2020-02-14 10:30:01 -0500

    exporting to PEM

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"log"
 )
 
@@ -19,4 +21,22 @@ func generateKeyPair(size int) (privateKey *rsa.PrivateKey, publicKey *rsa.Publi
 	publicKey = &privateKey.PublicKey
 
 	return privateKey, publicKey
+}
+
+func exportToPEM(privateKey *rsa.PrivateKey, publicKey *rsa.PublicKey) (privatePEM, publicPEM string) {
+	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	privatePEMBytes := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: privateKeyBytes})
+
+	privatePEM = string(privatePEMBytes)
+
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicPEMBytes := pem.EncodeToMemory(&pem.Block{Type: "RSA PUBLIC KEY", Bytes: publicKeyBytes})
+
+	publicPEM = string(publicPEMBytes)
+
+	return privatePEM, publicPEM
 }
